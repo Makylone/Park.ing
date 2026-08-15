05803f0ff71bec26d927a4bd1fca047077291bc6
Makylone <axel97477@gmail.com>
2026-08-15 21:58:25 +0200

    add a footer for the credits

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import UserEntry from "./components/UserEntry/index.tsx";
+import Footer from "./components/Footer/index.tsx";
 
 export default function App() {
   return (
     <>
       <div>
         <UserEntry />
+        <Footer />
       </div>
     </>
   );
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,31 @@
+import "./index.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__label">Credits</p>
+      <ul className="footer__list">
+        <li>Maaya for the formula (@./maaya_ou)</li>
+        <li>
+          <a
+            href="https://docs.google.com/spreadsheets/d/19KDNlfzjaOvS96kKe87xAfp2Rn5BHHpjVuVC9JqWbug"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Parking Spreadsheet
+          </a>{" "}
+          from Ai0
+        </li>
+        <li>
+          <a
+            href="https://docs.google.com/spreadsheets/d/1om--O7_NqvvQ6TDg1jrsjKMVBR_s1j1o"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            えび japanese sheet
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
